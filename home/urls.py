8ff2e057cbba367ff968f22b0677d3f49347f718
Chase Wrenn <chasewrenn@ufl.edu>
2022-07-04 12:41:16 -0400

    Created dynamic template

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('home', views.home),
-    path('authorized', views.authorized)
+    path('authorized', views.authorized),
 ]
