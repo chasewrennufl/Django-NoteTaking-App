1b3416d1d8ba48c63aed24727718882e1116d9d7
Chase Wrenn <chasewrenn@ufl.edu>
2022-07-12 11:05:26 -0400

    Implement Forms Api

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 from .models import Notes
+from .forms import NotesForm
 # Create your views here.
 
 class NotesListView(ListView):
@@ -12,5 +13,10 @@ class NotesListView(ListView):
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
+
+class NotesCreateView(CreateView):
+    model = Notes
+    form_class = NotesForm
+    success_url = '/smart/notes'
 
 
